401f5d63c7646d7bd032e45f48b3b5e137d8eae7
senha1senha1@ <admin@localhost.localdomain>
2019-09-03 11:17:57 -0300

    update scripts

--- build/nodejs-install.sh
+++ build/nodejs-install.sh
@@ -1,2 +1,3 @@
 cd $HOME/m2csea-nodejs-sample-projects/
 find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && npm install" \;
+rsync -avh --progress  $HOME/m2csea-nodejs-sample-projects/  $HOME/m2csea-engine/containers/applications/simulated/node
