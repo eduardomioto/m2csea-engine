a318c4aac956d8fe50570ff62daa64b43d0b1cb5
senha1senha1@ <admin@localhost.localdomain>
2019-09-03 11:26:26 -0300

    update scripts

---
+++ build/arch-probe-install.sh
@@ -0,0 +1,5 @@
+cd $HOME/m2csea-architecture-probe
+mvn clean install
+cp $HOME/m2csea-architecture-probe/target/architecture-probe-1.0.0.jar $HOME/m2csea-engine/containers/applications/architecture-probe
+
+
---
+++ build/finder-install.sh
@@ -0,0 +1,3 @@
+cd $HOME/m2csea-finder
+mvn clean install
+cp $HOME/m2csea-finder/target/finder-1.0.0.jar /home/admin/m2csea-engine/containers/applications/finder
