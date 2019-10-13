66707f3679e4cf4acbbcb57a2719dd03efb02e57
Eduardo Mioto <eduardo.mioto@philips.com>
2019-10-13 11:44:06 -0300

    update
    update

--- build/visualizer-install.sh
+++ build/visualizer-install.sh
@@ -2,5 +2,5 @@ cd $HOME/m2csea-visualizer
 git pull
 npm install
 sudo rm -R $HOME/m2csea-engine/containers/volumes/visualizer/
-sudo cp -R $HOME/m2csea-visualizer/static  $HOME/m2csea-engine/containers/volumes/visualizer/
+sudo cp -R $HOME/m2csea-visualizer/static/  $HOME/m2csea-engine/containers/volumes/visualizer/
 sudo chmod -R 777 $HOME/m2csea-engine/containers/volumes/visualizer/
